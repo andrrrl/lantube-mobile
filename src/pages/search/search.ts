da2d9746b0564c711c6b563a8e75516fc551e194
Andrrr <andresin@gmail.com>
2018-05-21 19:57:26 -0300

    Added search component

---
+++ src/pages/search/search.ts
@@ -0,0 +1,95 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController } from 'ionic-angular';
+import { YoutubeService } from '../../app/services/youtube.service';
+import { VideosService } from '../../app/services/videos.service';
+
+@Component({
+    selector: 'page-search',
+    templateUrl: 'search.html'
+})
+export class SearchPage {
+
+    public youtubeVideo: any;
+    public term = '';
+    public videoList: any[] = [];
+
+    loader: any;
+
+    constructor(
+        public navCtrl: NavController,
+        public alertCtrl: AlertController,
+        public loadingCtrl: LoadingController,
+        public youtubeService: YoutubeService,
+        public videosService: VideosService) {
+
+    }
+
+    search() {
+        this.showLoader('Buscando videos...');
+        this.videoList = [];
+        this.youtubeService.search(this.term).subscribe(result => {
+            this.videoList = result;
+            this.hideLoader();
+        });
+    }
+
+    view(id) {
+        return id;
+    }
+
+    showLoader(text: string) {
+        this.loader = this.loadingCtrl.create({
+            content: text,
+        });
+        this.loader.present();
+    }
+
+    hideLoader() {
+        this.loader.dismiss();
+    }
+
+    showConfirmAdd(index) {
+        let confirm = this.alertCtrl.create({
+            title: 'Agregar video?',
+            message: 'El video se va a agregar a la lista',
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'Aceptar',
+                    handler: () => {
+                        this.addVideo(index);
+                        console.log('Agree clicked');
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+    goBack() {
+        this.navCtrl.pop();
+        // this.viewCtrl.dismiss();
+    }
+
+    addVideo(index: number) {
+        this.youtubeVideo = this.selectVideo(index);
+        this.videosService.add(this.extractVideoId()).subscribe(video => {
+            this.navCtrl.pop();
+        });
+    }
+
+    selectVideo(index: number) {
+        return this.videoList[index];
+    }
+
+    extractVideoId() {
+        return this.youtubeVideo.url.trim().replace(/\/watch\?v=/, '');
+    }
+
+}
