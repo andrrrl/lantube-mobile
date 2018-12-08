445f9347b08b26769925f287a0c670052ff86b01
Andrés <andresin@gmail.com>
2018-12-07 22:40:31 -0300

    Removed unused imports

--- src/app/services/videos.service.ts
+++ src/app/services/videos.service.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Http, Response, ResponseContentType, RequestMethod, Headers } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import * as io from 'socket.io-client';
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,30 +1,30 @@
 import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Http, Response, ResponseContentType, RequestMethod, Headers, RequestOptions } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class YoutubeService {
 
-  constructor(private http: Http) { }
+    constructor(private http: Http) { }
 
-  // private API = location.href.includes('172.16') ? environment.API : (location.href.includes('192.168.4') ? environment.API_WIFI_CASA : environment.API_WIFI);
-  private API = environment.API_CASA ? environment.API_WIFI_CASA : (location.href.includes('172.16') ? environment.API : (location.href.includes('192.168.4') ? environment.API_WIFI_CASA : environment.API_WIFI));
+    // private API = location.href.includes('172.16') ? environment.API : (location.href.includes('192.168.4') ? environment.API_WIFI_CASA : environment.API_WIFI);
+    private API = environment.API_CASA ? environment.API_WIFI_CASA : (location.href.includes('172.16') ? environment.API : (location.href.includes('192.168.4') ? environment.API_WIFI_CASA : environment.API_WIFI));
 
-  search(term: string): Observable<any> {
-    return this.http.get(this.API + '/api/videos/search/' + encodeURIComponent(term)).map(this.extractData).catch(this.handleError);
-  }
+    search(term: string): Observable<any> {
+        return this.http.get(this.API + '/api/videos/search/' + encodeURIComponent(term)).map(this.extractData).catch(this.handleError);
+    }
 
-  private extractData(res: Response) {
-    return res.json();
-  }
+    private extractData(res: Response) {
+        return res.json();
+    }
 
-  private handleError(error: any) {
-    let errMsg = (error.message) ? error.message :
-      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
+    private handleError(error: any) {
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 
 }
