8f0584726fa78fb7bceda1ed8e2d8c316a918952
Andrés <andresin@gmail.com>
2018-12-07 22:48:16 -0300

    Code cleanup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,220 +5,126 @@ import { ServerService } from '../../app/services/server.service';
 import { PlayerService } from '../../app/services/player.service';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
+    // Current vol
+    currentVolume: any;
 
-  currentVolume: any;
-  addModal: Modal;
-  videosTmp: any[];
-  loader: any;
-  // Player configurations from the API
-  videoModes: any[] = [
-    'windowed',
-    'fullscreen',
-    'chromecast',
-    'audio only'
-  ];
+    // Spinner
+    loader: any;
 
-  // Server stats
-  public serverStats: any = {};
-  // Server stats
-  public playerStats: any = {
-    videoId: '',
-    status: 'idle'
-  };
+    // Server stats
+    public playerStats: any = {};
 
-  // Player settings
-  public player = {};
+    // List of all videos from the API
+    public videos = [];
 
-  // List of all videos from the API
-  public videos = [];
+    constructor(
+        public navCtrl: NavController,
+        public loadingCtrl: LoadingController,
+        public videosService: VideosService,
+        public serverService: ServerService,
+        public playerService: PlayerService,
+        public alertCtrl: AlertController,
+        public modalCtrl: ModalController) {
+    }
 
-  // Current selected video
-  public currentVideo: any = {};
+    ionViewDidLoad() {
+        if (typeof this.playerStats === 'undefined') {
+            this.getPlayerStats();
+        }
 
-  // New video to be added thru <input>
-  public videoURL: any;
+        this.playerService.onNewMessage().subscribe(stats => {
+            this.playerStats = stats;
+        });
+    }
 
-  // private EVENT_URL = environment.API + '/api/stats?type=player';
-  // private serverMessage: any = {};
-  // private eventSource: any;
+    async ionViewDidEnter() {
 
-  selectedItem: any;
-  icons: string[];
-  items: Array<{ title: string, note: string, icon: string }>;
+        this.showLoader('Cargando lista de videos...');
+        await this.loader.present();
 
-  public volumeRange: any = { upper: 100 };
+        this.videosService.onNewMessage().subscribe(videosMessage => {
+            if (videosMessage.message === 'getAll' || videosMessage.message === 'added' || videosMessage.message === 'deleted') {
+                this.getAllVideos();
+            }
+        });
+    }
+    /**
+       * Get full list of videos
+       */
+    getAllVideos() {
+        this.videosService.get({}).subscribe(videos => {
+            this.videos = videos.sort((a, b) => parseInt(b.videoId.replace(/video/, '')) - parseInt(a.videoId.replace(/video/, '')));
+            this.loader.dismiss();
+        });
+    }
 
-  constructor(
-    public navCtrl: NavController,
-    public loadingCtrl: LoadingController,
-    public videosService: VideosService,
-    public serverService: ServerService,
-    public playerService: PlayerService,
-    public alertCtrl: AlertController,
-    public modalCtrl: ModalController) {
-  }
+    playPause() {
+        this.playerService.playPause().subscribe();
 
+    }
 
-  ionViewDidLoad() {
-    // this.getPlayerStats();
-  }
+    async showLoader(text: string) {
+        this.loader = this.loadingCtrl.create({
+            content: text,
+        });
+    }
 
-  async ionViewDidEnter() {
+    async hideLoader() {
+        await this.loader.dismiss();
+    }
 
-    // this.getPlayerStats();
-    this.showLoader('Cargando lista de videos...');
-    await this.loader.present();
-    this.getAllVideos();
-
-    this.videosService.onNewMessage().subscribe(videosMessage => {
-      if (videosMessage.message === 'getAll' || videosMessage.message === 'added' || videosMessage.message === 'deleted') {
-        this.getAllVideos();
-      }
-    });
-    this.playerService.onNewMessage().subscribe(stats => {
-      this.playerStats = stats;
-    });
-
-  }
-  /**
-     * Get full list of videos
+    /**
+     * Get player stats (current volume, last video, etc)
      */
-  getAllVideos() {
-    this.videosService.get({}).subscribe(videos => {
-      this.videos = videos.sort((a, b) => parseInt(b.videoId.replace(/video/, '')) - parseInt(a.videoId.replace(/video/, '')));
-      this.videosTmp = this.videos;
-      this.loader.dismiss();
-    });
-  }
+    getPlayerStats() {
+        this.serverService.get().subscribe(stats => {
+            this.playerStats = stats;
+        });
 
-  /**
-   * Play selected video by ID
-   * @param id Redis or MongoDB video ID
-   */
-  async play(id) {
-    this.showLoader('Reproduciendo...');
-    await this.loader.present();
-    this.playerService.play(id).subscribe(async playback => {
-      this.currentVideo = playback;
-      this.getPlayerStats();
-      await this.loader.dismiss();
-    });
+    }
 
-  }
+    playPrev() {
+        this.playerService.playPrev().subscribe();
 
-  playPause() {
-    this.showLoader('Reproduciendo...');
-    this.playerService.playPause().subscribe(playback => {
-      this.currentVideo = playback;
-      this.playerService.onNewMessage().subscribe(stats => {
-        this.playerStats = stats;
-        this.hideLoader();
-      });
-    });
+    }
 
-  }
+    playNext() {
+        this.playerService.playNext().subscribe();
 
-  async showLoader(text: string) {
-    this.loader = this.loadingCtrl.create({
-      content: text,
-    });
-  }
+    }
 
-  async hideLoader() {
-    await this.loader.dismiss();
-  }
+    playCurrent() {
+        this.playerService.play(`video${this.videos.length - 1}`).subscribe();
+    }
 
-
-
-  /**
-     * Get server
+    /**
+     * Stops all playback
      */
-  getServerStats() {
-    this.serverService.get().subscribe(stats => {
-      this.serverStats = stats;
-    });
-  }
+    stopAll() {
+        this.playerService.stopAll().subscribe();
+    }
 
-  /**
-   * Get player stats (current volume, last video, etc)
-   */
-  getPlayerStats() {
-    this.serverService.get().subscribe(stats => {
-      this.playerStats = stats;
-      this.currentVideo.videoId = this.playerStats.videoId;
-    });
+    /**
+     * Pauses playback
+     */
+    pause() {
+        this.playerService.pause().subscribe(playback => {
+            this.playerStats.status = 'paused';
+        });
+    }
 
-  }
-
-  playPrev() {
-    this.showLoader('Reproduciendo...');
-    this.playerService.playPrev().subscribe(playback => {
-      this.currentVideo = playback;
-      this.playerService.onNewMessage().subscribe(stats => {
-        this.playerStats = stats;
-        this.hideLoader();
-      });
-      this.hideLoader();
-    });
-
-  }
-  playNext() {
-    this.showLoader('Reproduciendo...');
-    this.playerService.playNext().subscribe(playback => {
-      this.currentVideo = playback;
-      this.playerService.onNewMessage().subscribe(stats => {
-        this.playerStats = stats;
-        this.hideLoader();
-      });
-      this.hideLoader();
-    });
-
-  }
-
-  /**
-   * Plays PLS file generated "on the fly"
-   */
-  playAll() {
-    this.playerService.playAll().subscribe(playback => {
-      this.currentVideo = playback;
-    });
-  }
-
-  /**
-   * Stops all playback
-   */
-  stopAll() {
-    this.showLoader('Deteniendo reproducción...');
-    this.playerService.stopAll().subscribe(playback => {
-      this.currentVideo = playback;
-      this.playerStats.status = 'idle';
-      this.hideLoader();
-    });
-  }
-
-  /**
-   * Pauses playback
-   */
-  pause() {
-    this.playerService.pause().subscribe(playback => {
-      this.currentVideo = playback;
-      this.playerStats.status = 'paused';
-    });
-  }
-
-  /**
-   * Changes volume
-   */
-  volume(change) {
-    this.playerService.setVolume(change).debounceTime(200).subscribe(playback => {
-      this.currentVideo = playback;
-      this.currentVolume = change;
-    });
-  }
+    /**
+     * Changes volume
+     */
+    volume(change) {
+        this.playerService.setVolume(change).debounceTime(200).subscribe(playback => {
+            this.currentVolume = change;
+        });
+    }
 
 }
