1e067dfac1aa1c4775646045bf585195ab8bb847
Andrés <andresin@gmail.com>
2018-12-07 23:03:06 -0300

    omx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   // API: 'http://172.16.3.21:3000',
   // API: 'http://172.16.3.31:3000',
   // API: 'http://192.168.4.31:3000',
-  API_CASA: false,
+  API_CASA: true,
   API_WIFI_CASA: 'http://192.168.4.31:3000',
   API_WIFI: 'http://192.168.60.64:3000', // HPN WIFI
   API: 'http://172.16.3.127:3000', // HPN LAN
@@ -18,5 +18,5 @@ export const environment = {
   // API: 'http://192.168.1.131:3000',
   // API: 'http://192.168.1.108:3000', // Museo GFO
   // API: 'http://192.168.1.171:3000',
-  PLAYER: 'vlc'
+  PLAYER: 'omxplayer'
 };
