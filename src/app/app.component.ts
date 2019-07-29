8486358e9de578864d355148702ea9025ef47700
Andrés <andresin@gmail.com>
2019-07-29 19:47:56 -0300

    Refactored config settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,72 +9,77 @@ import { ListPage } from '../pages/list/list';
 import { AddPage } from '../pages/add/add';
 import { SearchPage } from '../pages/search/search';
 import { ConfigPage } from '../pages/config/config';
+import { ConfigService } from './services/config.services';
 
 @Component({
-  templateUrl: 'app.html',
+    templateUrl: 'app.html',
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = PlayerPage;
+    rootPage: any = PlayerPage;
 
-  pages: Array<{ title: string, component: any }>;
+    pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
-    public alertCtrl: AlertController) {
-    this.initializeApp();
+    constructor(public platform: Platform,
+        public configService: ConfigService,
+        public statusBar: StatusBar,
+        public splashScreen: SplashScreen,
+        public alertCtrl: AlertController) {
+        this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Videos', component: ListPage },
-      { title: 'Agregar URL', component: AddPage },
-      { title: 'Buscar', component: SearchPage },
-      { title: 'Configuraciones', component: ConfigPage },
-    ];
+        // used for an example of ngFor and navigation
+        this.pages = [
+            { title: 'Videos', component: ListPage },
+            { title: 'Agregar URL', component: AddPage },
+            { title: 'Buscar', component: SearchPage },
+            { title: 'Configuraciones', component: ConfigPage },
+        ];
 
-  }
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.nav.setRoot(PlayerPage);
-      this.splashScreen.hide();
-      this.platform.registerBackButtonAction(() => {
 
-        if (this.nav.canGoBack()) { //Can we go back?
-          this.nav.pop();
-        } else {
-          const alert = this.alertCtrl.create({
-            title: 'Salir de Lantube',
-            message: 'La App se va a cerrar',
-            buttons: [{
-              text: 'Cancelar',
-              role: 'cancel',
-              handler: () => {
-                console.log('Application exit prevented!');
-              }
-            }, {
-              text: 'Cerrar App',
-              handler: () => {
-                this.platform.exitApp(); // Close this application
-              }
-            }]
-          });
-          alert.present();
-        }
-        // this.nav.pop();
-      });
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            this.configService.autoConnect();
+            this.statusBar.styleDefault();
+            this.nav.setRoot(PlayerPage);
+            this.splashScreen.hide();
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.push(page.component);
-  }
+            this.platform.registerBackButtonAction(() => {
+
+                if (this.nav.canGoBack()) { //Can we go back?
+                    this.nav.pop();
+                } else {
+                    const alert = this.alertCtrl.create({
+                        title: 'Salir de Lantube',
+                        message: 'La App se va a cerrar',
+                        buttons: [{
+                            text: 'Cancelar',
+                            role: 'cancel',
+                            handler: () => {
+                                console.log('Application exit prevented!');
+                            }
+                        }, {
+                            text: 'Cerrar App',
+                            handler: () => {
+                                this.platform.exitApp(); // Close this application
+                            }
+                        }]
+                    });
+                    alert.present();
+                }
+                // this.nav.pop();
+            });
+        });
+    }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.push(page.component);
+    }
 
 }
--- src/app/services/config.services.ts
+++ src/app/services/config.services.ts
@@ -1,21 +1,42 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class ConfigService {
 
+    APIEndpoint = new BehaviorSubject<any>(null);
+
     constructor() { }
 
-    setAPIEndpoint(ip) {
-        return localStorage.setItem('ip', ip);
+    setAPIEndpoint(ip, port) {
+        const url = `http://${ip}:${port}`;
+        this.APIEndpoint.next({ url });
+        localStorage.setItem('url', url);
+        return url;
     }
 
     getAPIEndpoint() {
-        return localStorage.getItem('ip') || environment.API;
+        return localStorage.getItem('url') || this.APIEndpoint.getValue() || null;
     }
 
     delAPIEndpoint() {
-        return localStorage.removeItem('ip');
+        this.APIEndpoint.next(null);
+        localStorage.removeItem('url');
+    }
+
+    async autoConnect() {
+        if (!this.getAPIEndpoint()) {
+            if (window.location.hostname) {
+                const result = await this.setAPIEndpoint(window.location.hostname, 3000);
+                if (result) {
+                    return true;
+                } else {
+                    return null;
+                }
+            }
+        } else {
+            return true;
+        }
     }
 
 }
