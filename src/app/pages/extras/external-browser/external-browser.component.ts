38647465d5486a42fa74f344e03751b1bca70d98
Andrés Boc-Ho <andresin@gmail.com>
2021-03-27 10:23:54 -0300

    feat(extras): open in remote browser

--- src/app/pages/extras/external-browser/external-browser.component.ts
+++ src/app/pages/extras/external-browser/external-browser.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { SecurityContext } from '@angular/compiler/src/core';
 import { ExtrasService } from '../services/extras.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, map, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-external-browser',
@@ -11,15 +13,60 @@ import { ExtrasService } from '../services/extras.service';
 export class ExternalBrowserComponent implements OnInit {
 
     server: any;
+    url: URL;
+    urlPattern = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/;
+    invalidForm = true;
+
+    // Form Model
+    form: FormGroup = new FormGroup({
+        url: new FormControl(null, [Validators.required, Validators.pattern(this.urlPattern)]),
+        edit: new FormControl({ disabled: this.invalidForm }, Validators.required)
+    });
+
+    // 2 Observables
+    formStatus$: Observable<string>;
+    formChanges$: Observable<any>;
+
+    entryId: number;
 
     constructor(private sanitizer: DomSanitizer, private extrasService: ExtrasService) { }
 
     ngOnInit() {
+
+        this.form.get('url').valueChanges.subscribe(u => console.log('URL: ', u));
+
+        this.formChanges$ = this.form.valueChanges.pipe(
+            // Lee el valor sólo si el distinto al último
+            distinctUntilChanged((a, b) => a === b),
+            tap(value => console.log(value)),
+            map(v => console.log(v))
+        );
+
+        this.formStatus$ = this.form.statusChanges.pipe(
+            distinctUntilChanged((a, b) => a === b),
+            tap(value => console.log(value)),
+            map(status => status.toLowerCase())
+        );
+
+        this.extrasService.onNewMessage().subscribe(browserMessage => {
+            if (browserMessage.message === 'open') {
+
+            }
+        });
     }
 
-    sendURL(url) {
-        const sanitizedURL = this.sanitizer.sanitize(SecurityContext.URL, url);
-        this.extrasService.shareURL(sanitizedURL, {});
+    openInBrowser() {
+        if (this.form.valid) {
+            console.log(this.form);
+            const entry = this.form.value;
+            const sanitizedURL = this.sanitizer.sanitize(SecurityContext.URL, entry.url);
+            this.extrasService.sendURL(sanitizedURL).subscribe(result => {
+                console.log(result);
+            });
+        } else {
+            console.log(this.form);
+        }
     }
+
 
 }
--- src/app/pages/extras/services/extras.service.ts
+++ src/app/pages/extras/services/extras.service.ts
@@ -1,13 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 import { ENV } from '@app/env';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ExtrasService {
 
+    private socket: SocketIOClient.Socket;
     private API = `${ENV.apiURL}/browser`;
 
+    constructor(
+        private http: HttpClient
+    ) {
         this.socket = io(ENV.apiURL.replace('/api', ''));
+    }
+
+    sendURL(url) {
+        return this.http.post(this.API, { url });
+    }
+
+    // EMITTER
+    sendMessage(msg: string) {
+        this.socket.emit('URL_SHARE_MESSAGE', { message: msg });
+    }
+
+    // HANDLER
+    onNewMessage() {
+        return new Observable<any>(observer => {
+            this.socket.on('URL_SHARE_MESSAGE', msg => {
+                observer.next(msg);
+            });
+            this.socket.on('disconnect', () => {
+                this.socket.removeAllListeners();
+            });
+            this.socket.off('URL_SHARE_MESSAGE', this.onNewMessage);
+        });
+    }
 }
